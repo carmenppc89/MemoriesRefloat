81c77018d2b8e8f03da5c147348e74030cdd5d5d
Dury89 <carmenppc89@gmail.com>
2025-11-02 19:28:48 +0100

    DisplayMJCanya WIP - Coger obj, estableces siempre camara

--- Assets/Scripts/MiniJocs/Canya/DisplayMJCanya.cs
+++ Assets/Scripts/MiniJocs/Canya/DisplayMJCanya.cs
@@ -7,14 +7,41 @@ public class DisplayMJCanya : MonoBehaviour
     [SerializeField]
     private SO_Canya SO_Canya;
 
-    // Start is called before the first frame update
+    // Todos sus hijos a mano
+    [Header("Hijos")]
+    [SerializeField]
+    private Canvas m_canvas;
+    [SerializeField]
+    private GameObject m_limite;
+    [SerializeField]
+    private GameObject m_var;
+    [SerializeField]
+    private GameObject m_peix;
+    [SerializeField]
+    private GameObject m_marc;
+    [SerializeField]
+    private GameObject m_progress;
+    [SerializeField]
+    private GameObject m_progVar;
+    [SerializeField]
+    private GameObject m_progMarc;
+
+    private void OnEnable()
+    {
+        m_canvas.renderMode = RenderMode.ScreenSpaceCamera;
+        m_canvas.worldCamera = FindAnyObjectByType(typeof(Camera)) as Camera;
+    }
     void Start()
     {
 
     }
 
-    // Update is called once per frame
     void Update()
+    {
+
+    }
+
+    private void GetChildren()
     {
 
     }
