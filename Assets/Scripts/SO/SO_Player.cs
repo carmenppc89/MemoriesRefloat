32e63a56d6903f7b58fe37bb7724030baee7396e
Dury89 <carmenppc89@gmail.com>
2025-11-13 20:21:41 +0100

    SO_player ini

---
+++ Assets/Scripts/SO/SO_Player.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "SO_Player", menuName = "Scriptables/Player")]
+public class SO_Player : ScriptableObject
+{
+
+}
